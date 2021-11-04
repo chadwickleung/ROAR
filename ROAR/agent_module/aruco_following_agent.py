5300bb7d704ca2b89a228b581a06b964de18e708
Chadwick Leung <chadwickleung@berkeley.edu>
2021-11-03 19:00:58 -0700

    aruco following pid control

--- ROAR/agent_module/aruco_following_agent.py
+++ ROAR/agent_module/aruco_following_agent.py
@@ -5,6 +5,7 @@ from ROAR.configurations.configuration import Configuration as AgentConfig
 import cv2
 import cv2.aruco as aruco
 import numpy as np
+from collections import deque
 
 
 class ArucoFollowingAgent(Agent):
@@ -15,29 +16,16 @@ class ArucoFollowingAgent(Agent):
         self.arucoParam = aruco.DetectorParameters_create()
         self.tracking_id = 0
         self.marker_length = 0.005       # in meters
-        self.distance_threshold = 0.012  # in tvec unit
-        self.left_threshold = 0.0018
-        self.right_threshold = 0.013
 
-        self.steering = 0
+        self.distance_threshold = 0.04  # in tvec unit
+        self.center = 0
+        self.turn_threshold = 0.01
+        # self.left_threshold = -0.01     # 0.0018
+        # self.right_threshold = 0.01    # 0.013
+        self.steering_boundary = [-0.5, 0.5]
 
-        self.top_left = [0, 0] # [x, y]
-        self.top_right = [0, 0]
-        self.bottom_left = [0, 0]
-        self.bottom_right = [0, 0]
-
-        self.left_slope = (self.top_left[1] - self.bottom_left[1]) / (self.top_left[0] - self.bottom_left[0])
-        self.right_slope = (self.top_right[1] - self.bottom_right[1]) / (self.top_right[0] - self.bottom_right[0])
-
-        self.left_y_intercept = self.top_left[1] - self.left_slope * self.top_left[0]
-        self.right_y_intercept = self.top_right[1] - self.right_slope * self.top_right[0]
-
-        self.left_x_given_y = lambda y: (y - self.left_y_intercept) / self.left_slope - self.left_threshold
-        self.right_x_given_y = lambda y: (y - self.right_y_intercept) / self.right_slope + self.right_threshold
-
-
-
-
+        self._error_buffer = deque(maxlen=10)
+        self._dt = 0.03
 
     def run_step(self, sensors_data: SensorsData, vehicle: Vehicle) -> VehicleControl:
         super().run_step(sensors_data=sensors_data, vehicle=vehicle)
@@ -50,40 +38,52 @@ class ArucoFollowingAgent(Agent):
                                                                                self.front_rgb_camera.intrinsics_matrix,
                                                                                self.front_rgb_camera.distortion_coefficient)
                 x, y, z = tvec[0][0]
-                print(x,y,z)
-                # cv2.aruco.drawAxis(img,
-                #                    self.front_rgb_camera.intrinsics_matrix,
-                #                    self.front_rgb_camera.distortion_coefficient,
-                #                    rvec,
-                #                    tvec,
-                #                    self.marker_length)
+                print(x, y, z)
 
-                cv2.putText(img, f"{tvec}",
-                            (10, 50),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1,
-                            (0, 0, 255), 2)
-                # cv2.putText(img, f"{rvec}",
-                #             (10, 100),
-                #             cv2.FONT_HERSHEY_SIMPLEX,
-                #             1,
-                #             (0, 0, 255), 2)
+                if z < 0.05:
+                    # stopping criteria
+                    return VehicleControl(throttle=0, steering=0)
+                else:
+                    if x < self.center - self.turn_threshold:
+                        # if the car is to your left
+                        turn = self.run_in_series(x)
+                        print("Turn left")
+                        return VehicleControl(throttle=0.16, steering=turn)
+                    elif x > self.center + self.turn_threshold:
+                        # if the car is to your right
+                        turn = self.run_in_series(x)
+                        print("Turn right")
+                        return VehicleControl(throttle=0.16, steering=-turn)
+                    else:
+                        # reset error buffer
+                        self._error_buffer = deque(maxlen=10)
+                        return VehicleControl(throttle=0.16, steering=0)
 
-                cv2.imshow("img", img)
-                cv2.waitKey(1)
-
-                top_left, top_right, bottom_right, bottom_left = bbox[self.tracking_id][0]
-                if z < self.distance_threshold:
-                    print("Stop")
-                    return VehicleControl(throttle = 0, steering = 0)
-                elif top_left[0] < self.left_x_given_y(top_left[1]):
-                    print("Turn left")
-                    return VehicleControl(throttle = 0.2, steering = -1)
-                elif top_right[0] > self.right_x_given_y(top_right[1]):
-                    print("Turn right")
-                    return VehicleControl(throttle = 0.2, steering = 1)
 
         return self.vehicle.control
+
+    def run_in_series(self, x) -> float:
+        target_x = self.center
+        current_x = x
+
+        k_p, k_d = 20, -5
+        error = target_x - current_x
+
+        self._error_buffer.append(error)
+
+        if len(self._error_buffer) >= 2:
+            # print(self._error_buffer[-1], self._error_buffer[-2])
+            _de = (self._error_buffer[-2] - self._error_buffer[-1]) / self._dt
+        else:
+            _de = 0.0
+
+
+        output = float(np.clip((k_p * error) + (k_d * _de), self.steering_boundary[0],
+                               self.steering_boundary[1]))
+        # self.logger.debug(f"curr_speed: {round(current_speed, 2)} | kp: {round(k_p, 2)} | kd: {k_d} | ki = {k_i} | "
+        #       f"err = {round(error, 2)} | de = {round(_de, 2)} | ie = {round(_ie, 2)}")
+        # f"self._error_buffer[-1] {self._error_buffer[-1]} | self._error_buffer[-2] = {self._error_buffer[-2]}")
+        return output
 
     def findArucoMarkers(self):
         if self.front_rgb_camera.data is not None:
